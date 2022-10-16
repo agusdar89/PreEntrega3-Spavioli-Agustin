630a84ad4a9667dfc6e992667400c004fcbbf231
Agustin Spavioli <agusdar89@gmail.com>
2022-10-15 21:05:36 -0300

    se modifican secciones de Bootstrap y se corrigen acciones del boton NightVision

--- JS/main.js
+++ JS/main.js
@@ -3,5 +3,21 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('night');
     btnSwitch.classList.toggle('active');
-})
+
+    //guardado en modo localstorage.
+    if(document.body.classList.contains('night')){
+        localStorage.setItem('night-mode', 'true');
+    } else {
+        localStorage.setItem('night-mode', 'false');
+    }
+});
+
+//modo actual.
+if(localStorage.getItem('night-mode') === 'true'){
+    document.body.classList.add('night');
+    btnSwitch.classList.add('active');
+} else {
+    document.body.classList.remove('night');
+    btnSwitch.classList.remove('active');
+}
 
